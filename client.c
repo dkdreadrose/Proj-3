5f57b85cf445bc3a4f94aea4cb39dc55ca4fe0f2
Talon <35707707+dkdreadrose@users.noreply.github.com>
2019-10-07 13:35:08 -0500

    possibly completed client code
    need to write server code to be able to test this

--- client.c
+++ client.c
@@ -9,35 +9,57 @@
 
 #define SERVER_IP "000"
 #define SERVER_PORT 00
-#define DATA_SIZE 00
+#define DATA_SIZE 99
 
 
 int main(int argc, char *argv[]) {
-  char outMessage[99]; //the outgoing message, to be sent to the server
-  char inMessage[255]; //the incoming message, received from the server
-  int sd; //socket descriptor
+  char outMessage[DATA_SIZE]; //the outgoing message, to be sent to the server
+  char inMessage[DATA_SIZE]; //the incoming message, received from the server
+  int sd, nBytes; //socket descriptor, number bytes received from the server
   struct sockaddr_in server; //the server address
-  int rc; //return code (error if < 0)
+  int returnCode; //the return value of sendto()
+  int addrSize;
   
   //create the socket
-  sd = socket(AF_INET, SOCK_DGRAM, 0);
-  if(sd == -1) 
+  sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if(sd < 0) 
     errExit("Failed to create socket: %s", strerror(errno));
   
   //fill out server address structure
   server.sin_family = AF_INET;
   server.sin_port = htons(SERVER_PORT); // http uses port 80
-  inet_pton(AF_INET, SERVER_IP, &(server.sin_addr.s_addr));
+  server.sin_addr.s_addr = inet_addr(SERVER_IP);
+  memset(server.sin_zero, '\0', sizeof server.sin_zero); 
   
-  //get the outgoing message
-  printf("Message: ");
-  fgets(outMessage, 99, stdin);
-  outMessage[sizeof(outMessage)-1] = '\0'; //make it null-terminated
-  printf('Outgoing message: "%s"', outMessage);
+  addrSize = sizeof(server);
   
-  //TODO send the message using sendto(), and check the eror return value
+  //start loop to get message from user, send to server, and wait for response
+  while(1) {
+    //get the outgoing message
+    printf("Message: ");
+    fgets(outMessage, DATA_SIZE, stdin);
+    outMessage[sizeof(outMessage)-1] = '\0'; //make it null-terminated
+    printf('Outgoing message: "%s"', outMessage);
+    
+    //send the message using sendto(), and check the error return value
+    returnCode = sendto(sd, (char *)outMessage, DATA_SIZE, MSG_CONFIRM, (struct sockaddr *)&server, sizeof(server));
+    if(returnCode < 0) 
+      errExit("Failed to send message: %s", strerror(errno));
+    
+    //receive and process the message from the server
+    nBytes = recvfrom(sd, (char *) inMessage, DATA_SIZE, MSG_WAITALL, (struct sockaddr*) &server, &addrSize);
+    
+    if(nBytes > 0) {
+      inMessage[nBytes] = '\0';
+      printf("Server says: \"%s\"\n", inMessage);
+      break;
+    }
+    else {
+      printf("There was no reply from the server, please try again.\n"); 
+    }
+  }
   
-  //TODO receive and process the message from the server
-  
+  close(sd);
+
   return 0;
 }
