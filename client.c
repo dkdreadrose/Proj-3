d4afac78321f99aa6caa7a3d67289c87a407b75e
Talon <35707707+dkdreadrose@users.noreply.github.com>
2019-10-07 12:31:26 -0500

    upadted to get user's message

--- client.c
+++ client.c
@@ -1,1 +1,31 @@
+#include <sys/types.h>
+#include <arps/inet.h>
+#include <sys/socket.h>
 
+#include "client.h"
+
+#define SERVER_IP "000"
+#define SERVER_PORT 00
+#define DATA_SIZE 00
+
+
+int main(int argc, char *argv[]) {
+  char outMessage[99]; //the outgoing message, to be sent to the server
+  char inMessage[255]; //the incoming message, received from the server
+  
+  //create the socket
+  
+  //fill out server address structure
+  
+  //get the outgoing message
+  printf("Message: ");
+  fgets(outMessage, 99, stdin);
+  outMessage[sizeof(outMessage)-1] = '\0'; //make it null-terminated
+  printf('Outgoing message: "%s"', outMessage);
+  
+  //send the message using sendto(), and check the eror return value
+  
+  //receive and process the message from the server
+  
+  return 0;
+}
